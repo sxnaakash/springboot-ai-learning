8e6e1abed91c21ed581c17a2e75c08953dc2bfc7
Akash Saxena <akash048@gmail.com>
2026-06-02 23:04:53 +0530

    Created first Spring Boot REST API

---
+++ src/main/java/com/akash/springboothello/SpringBootHelloApplication.java
@@ -0,0 +1,13 @@
+package com.akash.springboothello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootHelloApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootHelloApplication.class, args);
+    }
+
+}
